32fc93d06152b0d9252e951d5a2bf4c947c95004
= <peterhjhan@gmail.com>
2018-11-07 23:11:42 +0000

    implement index, new, create for catogories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,38 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @catgories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+
+  def create 
+    @category = Category.new(category_params)
+    
+    if @category.save
+      redirect_to [:admin, :products], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+
   end
 
 end
