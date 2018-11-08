65e1cd94248720ee012a0ed8fad5f7e46ae2539c
= <peterhjhan@gmail.com>
2018-11-08 19:49:14 +0000

    apply controller create

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,19 @@ class VisitorsController < ApplicationController
   end
 
   def create
+    user = Visitor.new(user_params)
+    if user.save
+      session[:visiro_id] = visitor.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
   
+  private
+
+  def visitor_params
+    params.require(:visitor).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
