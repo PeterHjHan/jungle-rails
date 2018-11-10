3d7e1c69fd401e1a3ecf5f368f654874f74a21a8
= <peterhjhan@gmail.com>
2018-11-10 17:20:19 +0000

    generate scaffold

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,2 @@
 class Review < ActiveRecord::Base
-  validates :product_id, presence: true
-  validates :user_id, presence:true
-  validates :rating, numericality: { in: 1..5 }
 end
-
-
--- db/migrate/20181110171401_create_reviews.rb
+++ db/migrate/20181110171401_create_reviews.rb
@@ -3,12 +3,10 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.integer :product_id
       t.integer :user_id
-      t.text    :description
+      t.text :description
       t.integer :rating
 
       t.timestamps null: false
     end
   end
 end
-
-
