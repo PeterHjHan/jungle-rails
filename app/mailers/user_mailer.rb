9e96cfa20eb858d8f865bc0c042df7c439bb1d71
= <peterhjhan@gmail.com>
2018-11-11 00:16:41 +0000

    shows product specific ratings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
 
-  def order_email(order)
+def order_email(order)
   @order = order
   mail(to: "no-reply@jungle.com ", subect: "Receipt for your Order: #{@order.id}")
   end
