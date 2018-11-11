35fb4b531bfea4db12b057607ffb0cbe5a72de29
= <peterhjhan@gmail.com>
2018-11-11 05:04:05 +0000

    email formatting completed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ApplicationMailer
 
+  @line_items = LineItem.all
+
 def order_email(order)
   @order = order
   mail(to: "no-reply@jungle.com ", subect: "Receipt for your Order: #{@order.id}")
