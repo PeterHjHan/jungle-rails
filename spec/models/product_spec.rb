43e7cf6560be3aba399c973e776f5deee6d54dff
= <peterhjhan@gmail.com>
2018-11-12 21:01:07 +0000

    all working except category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  category = Category.new(id: 7, name: "TESTING")
+  subject {
+    described_class.new(name: "WHATSUP", price_cents: 10.00, quantity: 4, category_id: category.id)
+  }
+
   describe 'Validations' do
-    it "should have a valid name"
-    it "should have a price"
-    it "should have some quantities"
-    it "should have a category"
+
+    it "should have all the attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "should have not have a valid name" do
+      subject.name = nil;
+      expect(subject).to_not be_valid
+    end
+    it "should have not have a price" do
+      subject.price_cents = nil;
+      expect(subject).to_not be_valid
+    end
+
+    it "should have no quantities" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should have not have a category" do
+      puts "WHAT IS THIS? #{subject.category_id}"
+      subject.category_id = nil;
+      expect(subject).to_not be_valid
+    end
+
   end
 end
