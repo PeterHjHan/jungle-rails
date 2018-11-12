0c896852ad6fec583bf0b3e429c07b11b81895d6
= <peterhjhan@gmail.com>
2018-11-12 20:31:29 +0000

    create validations for rpsec or product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "should have a valid name"
+    it "should have a price"
+    it "should have some quantities"
+    it "should have a category"
+  end
+end
