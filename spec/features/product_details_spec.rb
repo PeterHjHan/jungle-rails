de96a9b133f17e926887c89bd99cfb2ce1425f28
= <peterhjhan@gmail.com>
2018-11-13 16:49:19 +0000

    complete rspec for product link

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
 
+  # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -13,16 +13,16 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
         quantity: 10,
         price: 64.99
       )
-    end
+
   end
 
-  scenario "They see all products" do
+  scenario "User shoud be able to click and redirect /product/:id" do
     # ACT
     visit root_path
+    click_link(@category.products.first.name)
 
     # DEBUG / VERIFY
     save_screenshot
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_text(@category.products.first.name)
   end
-
 end
